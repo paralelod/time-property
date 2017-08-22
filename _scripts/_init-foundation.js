c528fda1bd8a9a913d38ae2bb5fb3a6ed4a51c1c
paralelod <hello.diogorusso@gmail.com>
2017-08-21 23:57:21 -0300

    Update 2017-08-22T02:57:14.179Z

--- _scripts/_init-foundation.js
+++ _scripts/_init-foundation.js
@@ -1,6 +1,20 @@
 //foundation init
   $(document).foundation();
 
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var elements = document.getElementsByTagName("INPUT");
+//     for (var i = 0; i < elements.length; i++) {
+//         elements[i].oninvalid = function(e) {
+//             e.target.setCustomValidity("");
+//             if (!e.target.validity.valid) {
+//                 e.target.setCustomValidity("This field cannot be left blank");
+//             }
+//         };
+//         elements[i].oninput = function(e) {
+//             e.target.setCustomValidity("");
+//         };
+//     }
+// })
 
   // $(document).foundation({
   //   accordion: {
