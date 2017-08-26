e959814707716862e32e075680db334d32277bd8
paralelod <hello.diogorusso@gmail.com>
2017-08-26 00:14:37 -0300

    Update 2017-08-26T03:14:27.937Z

---
+++ _scripts/video.js
@@ -0,0 +1,40 @@
+
+//    $(document).on('open.zf.reveal', '[data-reveal]', function () {
+//     $('#time-video').attr('src', 'https://www.youtube.com/embed/qlxrei-wmyk?autoplay=1')
+//    }); 	
+
+
+$(document).ready(function() {
+    $('#video-btn').on('click', function(ev) {
+   
+      $("#time-video")[0].src += "?autoplay=1";
+      ev.preventDefault();
+   
+    });
+
+    $('.close-reveal-modal').on('click', function(ev) {
+        
+        $("#time-video")[0].src += "?autoplay=1";
+        ev.preventDefault();
+  });
+});
+
+
+
+// jQuery(".#time-video .triggerClose  ").live("click", function() {
+//     jQuery("#myModal iframe").attr("src", jQuery("#myModal iframe").attr("src"));
+// });
+
+// // Kills video on close button
+// $('#time-video .triggerClose').click(function() {
+//     $('#time-video').stopVideo();
+//     $('#time-video').attr('src', 'https://www.youtube.com/embed/qlxrei-wmyk')
+// });
+
+
+// // Kills video on overlay div
+// $('.reveal-overlay').click(function() {
+//     $('#time-video').stopVideo();
+//     $('#time-video').attr('src', 'https://www.youtube.com/embed/qlxrei-wmyk')
+    
+// });
